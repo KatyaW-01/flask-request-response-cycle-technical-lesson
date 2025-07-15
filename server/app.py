b294a47c67d49cdbd20a62e247e5bedff1e5ad38
Katya Weicht <katya.weicht@gmail.com>
2025-07-15 11:13:41 -0600

    Set up route and before request

--- server/app.py
+++ server/app.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
-
-from flask import Flask
+import os
+from flask import Flask, request, current_app, g
 
 app = Flask(__name__)
 
+@app.before_request
+def app_path():
+    g.path = os.path.abspath(os.getcwd())
+    #so our views all know where our application files are located
+
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    appname = current_app.name #provides information about the application we are working on 
+    return f'''
+    <h1>The host for this page is {host}</h1>
+    <h2> The name of this application is {appname} </h2>
+    <h3>The path of this application on the user's device is {g.path}</h3>
+    '''
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+ 
